5bcc80f3ff13e2049606f3d95d9a1368fb5e1f3c
Rosemaria Rosier <rosierosier@gmail.com>
2016-02-29 15:03:03 -0500

    completed assignment and removed dist from gitignore

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,3 +1,5 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
 // Hey Iron Yard Hackers! Enjoy!
 // Make sure to open your js consoles!
 
@@ -157,3 +159,5 @@ it("should make Faith cool and Dan not cool", function(){
   expect(faith.cool).toBe(true);
   expect(dan.cool).toBe(false);
 });
+
+},{}]},{},[1]);
